9d7db8a77f579e256a8f4a353028f853331bf165
Vanessa Furtunato <vanessafurtunato@Vanessas-MacBook-Pro.local>
2023-12-03 17:35:21 -0300

    First Resolver

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String
+      hi: String
     }
   `,
+  resolvers: {
+    Query: {
+      hello: async () => {
+        return 'hello';
+      },
+      hi: async () => {
+        return 'test';
+      },
+    },
+  },
 });
 
 server.listen(4003).then(({ url }) => {
